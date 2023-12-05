699ab77727e34c1761578980186d6ce33978629f
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-12-05 13:51:15 +0100

    docs: development and testing

--- examples/linux-app/main.tf
+++ examples/linux-app/main.tf
@@ -6,23 +6,18 @@ resource "random_id" "this" {
   byte_length = 8
 }
 
-resource "azurerm_resource_group" "this" {
-  name     = "rg-${random_id.this.hex}"
-  location = var.location
-}
-
 module "log_analytics" {
   source = "github.com/equinor/terraform-azurerm-log-analytics?ref=v1.5.0"
 
   workspace_name      = "log-${random_id.this.hex}"
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
+  resource_group_name = var.resource_group_name
+  location            = var.location
 }
 
 resource "azurerm_service_plan" "this" {
   name                = "plan-${random_id.this.hex}"
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
+  resource_group_name = var.resource_group_name
+  location            = var.location
   os_type             = "Linux"
   sku_name            = "Y1"
 }
@@ -31,8 +26,8 @@ module "storage" {
   source = "github.com/equinor/terraform-azurerm-storage?ref=v12.1.0"
 
   account_name                 = "st${random_id.this.hex}"
-  resource_group_name          = azurerm_resource_group.this.name
-  location                     = azurerm_resource_group.this.location
+  resource_group_name          = var.resource_group_name
+  location                     = var.location
   log_analytics_workspace_id   = module.log_analytics.workspace_id
   shared_access_key_enabled    = true
   network_rules_default_action = "Allow"
@@ -43,8 +38,8 @@ module "function_app" {
   source = "../.."
 
   app_name                   = "func-${random_id.this.hex}"
-  resource_group_name        = azurerm_resource_group.this.name
-  location                   = azurerm_resource_group.this.location
+  resource_group_name        = var.resource_group_name
+  location                   = var.location
   app_service_plan_id        = azurerm_service_plan.this.id
   storage_account_name       = module.storage.account_name
   storage_account_key        = module.storage.primary_access_key
