18479344999028b3be70ae1a12c176dfcc35e10c
kov117 <126077239+kov117@users.noreply.github.com>
2025-03-17 10:34:47 +0100

    refactor: removed ref from main.tf

--- main.tf
+++ main.tf
@@ -62,7 +62,6 @@ resource "azurerm_linux_function_app" "this" {
   }
 
   site_config {
-    # Ref: https://learn.microsoft.com/en-us/azure/azure-monitor/app/migrate-from-instrumentation-keys-to-connection-strings
     application_insights_key               = var.application_insights_key
     application_insights_connection_string = var.application_insights_connection_string
 
@@ -210,7 +209,6 @@ resource "azurerm_windows_function_app" "this" {
   }
 
   site_config {
-    # Ref: https://learn.microsoft.com/en-us/azure/azure-monitor/app/migrate-from-instrumentation-keys-to-connection-strings
     application_insights_key               = var.application_insights_key
     application_insights_connection_string = var.application_insights_connection_string
 
